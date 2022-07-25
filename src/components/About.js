c56771e736278388154305b4746a268f2319da46
devontehillman <hillmand@mail.gvsu.edu>
2022-07-25 15:02:06 -0400

    fixing media qurey

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ export default class About extends Component {
     render() {
         return (
             <div className="about-section">
-                <h1 style= {{id:'nametitle',fontFamily: 'poppins',fontWeight: 800, fontSize:'60px'}}>Devonte Hillman</h1>
+                <h1 id="nametitle" style= {{id:'nametitle',fontFamily: 'poppins',fontWeight: 800, fontSize:'60px'}}>Devonte Hillman</h1>
                 <p style= {{fontFamily: 'poppins'}}>My desire to program started in my first engineering class in college when we used C to create a self scoring Arduino robot. Since then I have attended a bootcamp to lean Full stack web development. In the future I plan to use my knowledge to work in the industry of machine learning and the internet of things because I have a strong desire to use computer programming to help automate everyday processes.</p>
                 <div className="socials">
                     <a href="https://github.com/devontehillman">
